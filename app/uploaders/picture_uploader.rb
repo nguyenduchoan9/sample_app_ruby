8970dc1ad239d25324dbaaefec4c0ad5d9691c9f
nguyenduchoan9 <nguyendhoan9@gmail.com>
2017-03-26 09:21:26 +0700

    fix heroku

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,17 +8,17 @@ class PictureUploader < CarrierWave::Uploader::Base
     # include CarrierWave::MiniMagick
 
     # Choose what kind of storage to use for this uploader:
-    if Rails.env.production?
-        storage :fog
-    else
-        storage :file
-    end
+    storage :fog
+    # if Rails.env.production?
+    # else
+    #     storage :file
+    # end
     # storage :fog
 
     # Override the directory where uploaded files will be stored.
     # This is a sensible default for uploaders that are meant to be mounted:
     def store_dir
-        "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+        "sampletirus/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
 
     # Add a white list of extensions which are allowed to be uploaded.
